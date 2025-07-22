cba80ae27cbcf2a0be9c4b78597be809e544c771
Javier Huertas <javiandre2007@gmail.com>
2025-07-22 09:48:36 -0600

    opciones (opcion1)

--- act05.py
+++ act05.py
@@ -1,4 +1,22 @@
-print("--------MENU DE VENTAS--------\n"
-      "1.Ingresar lista de valores\n"
-      "2. Mostrar las ventas\n"
-      "3. calcular la ")
+while True:
+      print("--------MENU DE VENTAS--------\n"
+            "1.Ingresar lista de valores\n"
+            "2. Mostrar las ventas\n"
+            "3. calcular la venta mas alta y la venta mas baja\n"
+            "4. calcular promedio\n"
+            "5. contar cuantos dias superaron los Q1000\n"
+            "6. Buscar una venta especifica\n"
+            "7. clasificar cada venta\n"
+            "8. Salir ")
+      option = input("Ingrese una opcion: ")
+      match option:
+            case "1":
+                  n = int(input("Ingrese la lista de ventas: "))
+                  for i in range(n):
+
+
+
+
+
+
+
